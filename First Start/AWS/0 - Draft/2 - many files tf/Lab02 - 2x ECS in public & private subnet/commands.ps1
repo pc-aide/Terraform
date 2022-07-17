9f55308a1aa4eae56b4be11d50720f9dfdbd0488
pc-aide <36453201+pc-aide@users.noreply.github.com>
2022-07-17 19:28:39 -0400

    Create commands.ps1

---
+++ First Start/AWS/0 - Draft/2 - many files tf/Lab02 - 2x ECS in public & private subnet/commands.ps1
@@ -0,0 +1,20 @@
+# Creds
+aws configure --profile "Lab"
+
+# Init
+terraform.exe init
+
+# Validate
+terraform.exe validate
+
+# code format
+terraform.exe fmt -recursive
+
+# Plan
+terraform.exe plan -out "lab.tfplan"
+
+# Deploy
+terraform.exe apply "lab.tfplan"
+
+# Destroy
+terraform.exe destroy -auto-approve
