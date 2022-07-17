f0443e94987c517a0622ec018f97e18d85c4d3c2
pc-aide <36453201+pc-aide@users.noreply.github.com>
2022-07-17 19:29:04 -0400

    Create instances.tf

---
+++ First Start/AWS/0 - Draft/2 - many files tf/Lab02 - 2x ECS in public & private subnet/instances.tf
@@ -0,0 +1,30 @@
+###############################################################################
+# DATA
+###############################################################################
+
+
+###############################################################################
+# RESOURCES
+###############################################################################
+
+# Instances
+# public_instances
+resource "aws_instance" "nginx" {
+  # EC2
+  count         = var.instance_count
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  # Network
+  # % -> modulo (math)
+  subnet_id              = module.vpc.public_subnets[(count.index % var.subnet_count)]
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  # bootstrap
+  # if we change code, so need to destroy to have new changes ?
+  user_data = file("./userdata.sh")
+
+  tags = local.common_tags
+}
+
+# private_instances
