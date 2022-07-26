50a3b57cf0eaaf200bdaf292b5dd620a9b17cf93
pc-aide <36453201+pc-aide@users.noreply.github.com>
2022-07-26 18:44:45 -0400

    Update network.tf

--- First Start/AWS/0 - Draft/2 - many files tf/Lab02 - alb/network.tf
+++ First Start/AWS/0 - Draft/2 - many files tf/Lab02 - alb/network.tf
@@ -16,8 +16,8 @@ module "vpc" {
   cidr                 = var.cidr_10_56
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  azs            = slice(data.aws_availability_zones.azs.names, 0, var.subnet_count)
-  public_subnets = [for subnet in range(var.subnet_count) : cidrsubnet(var.cidr_10_56, 8, subnet)]
+  azs             = slice(data.aws_availability_zones.azs.names, 0, var.subnet_count)
+  public_subnets  = [for subnet in range(var.subnet_count) : cidrsubnet(var.cidr_10_56, 8, subnet)]
   private_subnets = var.private_subnets
 
   enable_nat_gateway = true
