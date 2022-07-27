e7da263b785b0d1974715bb4738ddba335bba0b6
pc-aide <36453201+pc-aide@users.noreply.github.com>
2022-07-27 10:08:16 -0400

    Update providers.tf

--- First Start/AWS/0 - Draft/2 - many files tf/Lab02 - alb/providers.tf
+++ First Start/AWS/0 - Draft/2 - many files tf/Lab02 - alb/providers.tf
@@ -3,6 +3,7 @@
 ###############################################################################
 
 terraform {
+  # required_version = "~>1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
